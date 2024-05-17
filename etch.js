cf69ac3b5251006db4d6f84327f5c48892e9b401
Md Ataullah <106423293+kolasi11@users.noreply.github.com>
2024-05-17 12:22:42 +0530

    added hover effect to the canva

--- etch.js
+++ etch.js
@@ -1,19 +1,34 @@
 function grid() {
-    const conatiner = document.querySelector("#container");
+    const conatiner = document.querySelector(".container");
     let i = 1;
     const grids = [];
 
     for (let i = 0; i < 16; i++) {
         grids[i] = [];
         grids[i] = document.createElement('div');
-        grids[i].classList.add('row' + i)
+        grids[i].classList.add('row')
         for (let j = 0; j < 16; j++) {
             grids[i][j] = document.createElement('div');
-            grids[i][j].classList.add('grid' + i + j);
-            grids[i][j].textContent = "grid" + i + j;
+            const gridId = 'g_' + i + '_' + j;
+            grids[i][j].setAttribute('id', gridId);
+            grids[i][j].classList.add('grid');
             grids[i].appendChild(grids[i][j]);
         }
         conatiner.appendChild(grids[i]);
     }
 }
+
+function hover_effect() {
+const grids = document.getElementsByClassName("grid");
+    for (let ids of grids) {
+        ids.addEventListener(
+        'mouseover',
+        (event) => {
+            event.target.style.backgroundColor = 'black';
+        }
+        );
+    }
+}
+
 grid();
+hover_effect();
